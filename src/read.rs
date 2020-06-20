dad5ef7c88c153e9cb3413f1d8713f923bf19569
Arthur Pichlkostner <apichlkostner@gmx.de>
2020-06-20 23:08:40 +0200

    Multithreading

--- src/read.rs
+++ src/read.rs
@@ -1,9 +1,25 @@
 use crate::CHUNK_SIZE;
 use std::fs::File;
 use std::io::{self, BufReader, Read, Result};
+use std::sync::{Arc, Mutex};
 
 pub struct Reader {
     pub reader: Box<dyn Read>,
+}
+
+pub fn read_loop(infile: &str, quit: Arc<Mutex<bool>>) -> Result<()> {
+    let mut reader = Reader::new(infile)?;
+    loop {
+        let buffer = match reader.read() {
+            Ok(x) if x.is_empty() => break,
+            Ok(x) => x,
+            Err(_) => break,
+        };
+    }
+
+    let mut quit = quit.lock().unwrap();
+    *quit = true;
+    Ok(())
 }
 
 impl Reader {
@@ -16,7 +32,8 @@ impl Reader {
 
         Ok(Self { reader })
     }
-    pub fn read(&mut self) -> Result<Vec<u8>> {
+
+    fn read(&mut self) -> Result<Vec<u8>> {
         let mut buffer = [0; CHUNK_SIZE];
         let num_read = self.reader.read(&mut buffer)?;
 
